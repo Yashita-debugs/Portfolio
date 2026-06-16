3aca9fa0dd309398988697bfe94adf568e674fa3
Yashita <yashita.2502020017@muj.manipal.edu>
2026-06-16 18:22:35 +0530

    adding color change upon scrolling

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// const navBar=document.querySelector(".nav-bar");
+
+//  window.addEventListener('scroll', function() 
+//  {
+
+//  if(window.scrollY>50)
+//     { navBar.classList.add('scroll')
+
+//     }
+//     else
+//         {navBar.classList.remove('scroll')
+//         }
+//  })
+const navBar = document.querySelector(".nav-bar");
+
 window.addEventListener('scroll', function() {
-    console.log(window.scrollY) // prints the number as you scroll
+    if(window.scrollY > 50) {
+        navBar.classList.add('scroll')
+    } else {
+        navBar.classList.remove('scroll')
+    }
 })
